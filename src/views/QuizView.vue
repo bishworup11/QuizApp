8e487ce957e2cd3330b706c8e3c3871b84b51760
Bishworup Mollik <bishworup@appscode.com>
2025-02-25 10:37:29 +0600

    Add Result component and integrate it into QuizView for displaying quiz results

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,16 +1,18 @@
 <script setup>
     import Question from "../components/Question.vue"
     import QuizHeader from "../components/QuizHeader.vue"
+    import Result from "@/components/Result.vue"
     import { useRoute } from 'vue-router'
     import { ref, computed, watch } from 'vue' 
     import quizes from '../data/quizzes.json' 
+    import {RouterLink} from "vue-router"
 
      const route = useRoute()
      const quizId = parseInt(route.params.id);
      const quiz = quizes.find(q => q.id === quizId)
      const currentQuestionIndex = ref(0);
       const numberOfCorrectAnswers = ref(0)
-    // const showResults = ref(false)
+      const showResults = ref(false)
 
     //  const questionStatus = ref(`${currentQuestionIndex.value}/${quiz.questions.length}`)
 
@@ -28,8 +30,9 @@
         if(isCorrect){
             numberOfCorrectAnswers.value++
         }
+
         if(quiz.questions.length - 1 === currentQuestionIndex.value){
-            // showResults.value = true
+             showResults.value = true
             console.log("show result")
         }
         else{
@@ -62,15 +65,16 @@
                 :question="quiz.questions[currentQuestionIndex]"
                 @selectOption="onOptionSelected" -->
             <Question   
+             v-if="!showResults"
             :question="quiz.questions[currentQuestionIndex]"
             @selectOption="onOptionSelected"
             />
-            <!-- <Result 
+            <Result 
                 v-else
                 :quizQuestionLength="quiz.questions.length"
                 :numberOfCorrectAnswers="numberOfCorrectAnswers"
-            /> -->
+            />
         </div>
-        <button @click="currentQuestionIndex++">Next</button>
+        <RouterLink to="/">Go Back</RouterLink>
     </div>
 </template>
